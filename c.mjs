a39675770763043bf85d5c489c351130055599c3
Seqret1337 <alexandernordvaag@gmail.com>
2025-02-02 16:16:12 +0100

    Made function and it passed test n = 0-20.

--- c.mjs
+++ c.mjs
@@ -11,7 +11,22 @@ import test from "./test.mjs";
 */
 
 function sequence(n) {
+    if (n === 0) {
+        return 0;
+    }
+    if (n === 1 || n === 2) {
+        return 1;
+    }
 
+    let a = 1;
+    let b = 1;
+
+    for (let i = 3; i <= n; i++) {
+        const c = a + b;
+        a = b;
+        b = c;
+    }
+    return b;
 }
 
 //#region Tests --------------------------------------------------------------------
